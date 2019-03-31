01358eaaf830def1c6e6216e850c059975439256
Zac Long <zzl0100@tigermail.auburn.edu>
2019-03-31 13:57:09 -0400

    Delete existing Form and add new UIMainForm to WebBrowser.UI

--- WebBrowser.UI/UIMainForm.cs
+++ WebBrowser.UI/UIMainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WebBrowser.UI
 {
-    public partial class Form1 : Form
+    public partial class UIMainForm : Form
     {
-        public Form1()
+        public UIMainForm()
         {
             InitializeComponent();
         }
