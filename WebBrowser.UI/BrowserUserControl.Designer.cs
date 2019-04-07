8485de241a8152e31c8ac13675c97ce615853321
Zac Long <zzl0100@tigermail.auburn.edu>
2019-04-06 20:06:54 -0400

    Resize UserControl

--- WebBrowser.UI/BrowserUserControl.Designer.cs
+++ WebBrowser.UI/BrowserUserControl.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // BrowserUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "BrowserUserControl";
+            this.Size = new System.Drawing.Size(529, 354);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
